194ac7a4779eee9fb5bba1fced20351f8976af30
Minh <anhtm.mtl@gmail.com>
2020-04-06 21:43:39 -0400

    populate remaining sb objects; remove unused assets

---
+++ Assets/Scripts/Managers/MovementManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour
+{
+    private Axis axisLock;
+
+    private void Update()
+    {
+        
+    }
+}
